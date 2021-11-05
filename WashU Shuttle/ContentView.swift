af7efbf21e9a630f0ff7e4aea19d68624e766111
Michael Filippini <michaelfilippini20@gmail.com>
2021-11-04 20:32:50 -0500

    Initial Commit

---
+++ WashU Shuttle/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WashU Shuttle
+//
+//  Created by Michael Filippini on 11/4/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WashU Shuttle/WashU_ShuttleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WashU_ShuttleApp.swift
+//  WashU Shuttle
+//
+//  Created by Michael Filippini on 11/4/21.
+//
+
+import SwiftUI
+
+@main
+struct WashU_ShuttleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
