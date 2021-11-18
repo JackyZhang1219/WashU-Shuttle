81574b0eb69ac7a725c94f2c78cebdcca896e622
JackyZhang1219 <zhangzhengxi1219@gmail.com>
2021-11-18 13:52:35 -0600

    Add CoreLocation and MKDirections

---
+++ WashU Shuttle/GetWalkingRoute.swift
@@ -0,0 +1,52 @@
+//
+//  GetWalkingRoute.swift
+//  WashU Shuttle
+//
+//  Created by Jacky Zhang on 11/18/21.
+//
+
+import Foundation
+import MapKit
+
+class GetWalkingRoute: NSObject {
+    let request = MKDirections.Request()
+    
+    func findWalkingRoute (start: MKMapItem?, end: MKMapItem?, needsAlternateRoutes: Bool, transportType: MKDirectionsTransportType, routeMap: MKMapView!) {
+        request.source = start
+        request.destination = end
+        request.requestsAlternateRoutes = needsAlternateRoutes
+        request.transportType = transportType
+        let map = routeMap
+    
+        let directions = MKDirections(request: request)
+
+        directions.calculate { [unowned self] response, error in
+            guard let unwrappedResponse = response else { return }
+            
+            self.showRoute(unwrappedResponse, routeMap: map)
+        }
+    }
+        
+    func showRoute(_ response: MKDirections.Response, routeMap: MKMapView!) {
+        
+        for route in response.routes {
+            
+            routeMap.addOverlay(route.polyline,
+                         level: MKOverlayLevel.aboveRoads)
+            
+            for step in route.steps {
+                print(step.instructions)
+            }
+        }
+        
+    }
+
+    func mapView(_ mapView: MKMapView, rendererFor
+            overlay: MKOverlay) -> MKOverlayRenderer {
+        let renderer = MKPolylineRenderer(overlay: overlay)
+
+        renderer.strokeColor = UIColor.blue
+        renderer.lineWidth = 5.0
+        return renderer
+    }
+}
---
+++ WashU Shuttle/TestCurrentLocation.swift
@@ -0,0 +1,37 @@
+//
+//  TestCurrentLocation.swift
+//  WashU Shuttle
+//
+//  Created by Jacky Zhang on 11/17/21.
+//
+
+import SwiftUI
+
+struct MyView: View {
+    
+    @StateObject var locationManager = LocationManager()
+    
+    var userLatitude: String {
+        return "\(locationManager.lastLocation?.coordinate.latitude ?? 0)"
+    }
+    
+    var userLongitude: String {
+        return "\(locationManager.lastLocation?.coordinate.longitude ?? 0)"
+    }
+    
+    var body: some View {
+        VStack {
+            Text("location status: \(locationManager.statusString)")
+            HStack {
+                Text("latitude: \(userLatitude)")
+                Text("longitude: \(userLongitude)")
+            }
+        }
+    }
+}
+
+struct MyView_Previews: PreviewProvider {
+    static var previews: some View {
+        MyView()
+    }
+}
