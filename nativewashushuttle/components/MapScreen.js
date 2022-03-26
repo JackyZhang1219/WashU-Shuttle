a8deef8a3b1a97e8207dc563ed92d081c12153b8
udirose <udirose@wustl.edu>
2022-03-26 12:45:31 -0500

    cleaned up comments

--- nativewashushuttle/components/MapScreen.js
+++ nativewashushuttle/components/MapScreen.js
@@ -15,34 +15,6 @@ const styles = StyleSheet.create({
     },
 });
 
-// function constructor(props){
-//     super(props)
-//     this.state = {
-//       latitude: 0,
-//       longitude: 0,
-//       error: null
-//     }
-//   }
-
-//    function componentDidMount(){
-//     navigator.geolocation.getCurrentPosition(position =>{
-//         this.setState({
-//             latitude: position.coords.latitude,
-//             longitude: position.coords.longitude,
-//             error: null
-//         });
-//     }, error => this.setState({error:error.message}), {enableHighAccuracy: true, timeout: 20000, maximumAge: 20000}
-//     );
-//   }
-
-// useEffect(() => {
-//     if (Platform.OS === 'ios') {
-//       Geolocation.requestAuthorization('always');
-//     }
-// }, []);
-
-
-
 export default function MapScreen  () {
     return (
     <View style={styles.container}>
@@ -63,42 +35,3 @@ export default function MapScreen  () {
     );
  }
 
-
- // const UserLocation = () => {
-//     const [location, setLocation] = useState("")
-//     //https://dev-yakuza.posstree.com/en/react-native/react-native-maps/#display-user-location
-//     useEffect(() => {
-//         Geolocation.getCurrentPosition(
-//           position => {
-//             const {latitude, longitude} = position.coords;
-//             setLocation({
-//               latitude,
-//               longitude,
-//             });
-//           },
-//           error => {
-//             console.log(error.code, error.message);
-//           },
-//           {enableHighAccuracy: true, timeout: 15000, maximumAge: 10000},
-//         );
-//       }, []);
-
-//     return (
-//         <View style={styles.container}>
-//             {location && (
-//                 <MapView
-//                     style={styles.map}
-//                     region={{
-//                     latitude: location.latitude,
-//                     longitude: location.longitude,
-//                     latitudeDelta: 0.015,
-//                     longitudeDelta: 0.0121,
-//                     }}
-//                 >
-//                     <Marker coordinate={{latitude: location.latitude, longitude: location.longitude}}/>
-//                 </MapView>
-//             )}
-//         </View>
-//         );
-// }
-//export default UserLocation;
