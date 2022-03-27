b7470850c2a7319ae1637743eb30fb1a433f948f
udirose <udirose@wustl.edu>
2022-03-27 15:36:36 -0500

    moved mapview into seperate componenet to clean up App.js

--- nativewashushuttle/App.js
+++ nativewashushuttle/App.js
@@ -1,80 +1,26 @@
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View } from 'react-native'
 import React, {Component} from 'react'
-//import MapScreen from './components/MapScreen'
+import MapScreen from './components/MapScreen'
 import Pathfind from './components/Pathfind'
 import Search from './components/Search'
 import * as Location from 'expo-location'
-import MapView, {Marker} from 'react-native-maps'; 
+
 
 
 
 export default class App extends Component {
 
-  constructor(props){
-    super(props)
-    this.state = {
-      elements: [],
-      south: null,
-      west: null,
-      north: null,
-      east: null,
-      latitude: 38.648987,
-      longitude: -90.312553,
-    }
-  }
-
-  updateState(location) {
-    this.setState({
-      ...this.state,
-      latitude: location.coords.latitude,
-      longitude: location.coords.longitude,
-    });
-  }
-
-  async componentDidMount(){
-    try {
-      let { status } =  await Location.requestForegroundPermissionsAsync();
-      if (status !== 'granted') {
-        return;
-      }
-      let location =  await Location.getCurrentPositionAsync({});
-      this.updateState(location);
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
   render(){
     return (
       <View style={styles.container}>
-        <MapView
-              style={styles.map}
-              region={{
-              latitude: this.state.latitude,
-              longitude: this.state.longitude,
-              latitudeDelta: 0.015,
-              longitudeDelta: 0.0121,
-              }}>
-
-            <Marker coordinate={this.state} pinColor = {"lightblue"} // any color
-            title={"Current Location"}
-            />
-
-            <Marker coordinate={{latitude: 38.647445,longitude: -90.309686}} pinColor = {"red"} // any color
-            title={"Mallinckrodt"}
-            />
-                
-            <Pathfind />
-
-        </MapView>
-        <Search/>
+        <MapScreen/>
+        {/* <Search/> */}
         <StatusBar style="auto" />
       </View>
     )
   }
 
-  
 }
 
 
